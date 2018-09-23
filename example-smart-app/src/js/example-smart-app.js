d29ec34f50aa292896a114d88ecb0e2f4751bd9c
Matthew Storer <storer@ohsu.edu>
2018-09-23 16:13:30 -0700

    edited LOINC codes in example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -27,7 +27,7 @@
           type: 'Observation',
           query: {
             code: {
-              $or: ['http://loinc.org|26478-8', 'http://loinc.org|2345-7']
+              $or: ['http://loinc.org|2345-7', 'http://loinc.org|26478-8']
             }
           }
         });
